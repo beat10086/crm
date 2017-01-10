6d91f3fbaa85b5c52facb0a6778e8a93f9b520cc
caolei <caolei@ilongyuan.com.cn>
2017-01-10 15:51:43 +0800

    初始化thinkphp

---
+++ Application/Admin/Controller/IndexController.class.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zmz
+ * Date: 2017/1/10
+ * Time: 15:45
+ */
+namespace  Admin\Controller;
+use Think\Controller;
+
+class IndexController extends  Controller {
+      public  function index () {
+             echo '显示后台系统';
+      }
+}
